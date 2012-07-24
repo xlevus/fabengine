6110fc0e2e1e6bb9a1fc8e680b88e0f7d420db28
Chris Targett <xin@xlevus.net>
2012-07-24 12:41:13 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fabengine',
+      version='0.1',
+      description='Fabric commands for appengine.',
+      author='Chris Targett',
+      author_email='chris@xlevus.net',
+      url='http://github.com/xlevus/fabengine',
+      py_modules = ['fabengine']
+     )
