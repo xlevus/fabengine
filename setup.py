5ef17129ee79ce02196210e071c08feaf2289d19
Chris Targett <xin@xlevus.net>
2012-07-24 12:57:08 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,29 @@
 
 from distutils.core import setup
 
+import os
+def read(fname):
+     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name='fabengine',
-      version='0.1',
-      description='Fabric commands for appengine.',
-      author='Chris Targett',
-      author_email='chris@xlevus.net',
-      url='http://github.com/xlevus/fabengine',
-      py_modules = ['fabengine']
-     )
+     version='0.1',
+     description='Fabric commands for appengine.',
+     long_descriotion=read('README.md'),
+     author='Chris Targett',
+     author_email='chris@xlevus.net',
+     url='http://github.com/xlevus/fabengine',
+     py_modules = ['fabengine'],
+     install_requires = ['Fabric>=1.4.3','nose>=1.1.2','NoseGAE>=0.2.0'],
+     classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python",
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "Environment :: Console",
+          "Topic :: Software Development",
+          "Topic :: System :: Software Distribution",
+     ],
+     keywords='fabric appengine deployment',
+     license='',
+
+)
